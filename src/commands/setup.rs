c2cf3dec2d02ab24ae8926da467d8f59eded263d
AceREx <ace.rexnoir@gmail.com>
2026-06-18 00:06:21 +0530

    fix setup command to accept optional subcommand

--- src/commands/setup.rs
+++ src/commands/setup.rs
@@ -1,35 +1,42 @@
 use anyhow::{Ok, Result};
 use clap::{Args, Subcommand};
+use uzers::get_effective_uid;
 
 use crate::setup::{Caddy, Dns};
 
 #[derive(Debug, Args)]
 pub struct SetupArgs {
     #[command(subcommand)]
-    pub command: SetupCommands,
+    pub command: Option<SetupCommand>,
 }
+
 #[derive(Debug, Subcommand)]
-pub enum SetupCommands {
+pub enum SetupCommand {
     Dns,
     Caddy,
-    All,
 }
 
 pub fn run(args: SetupArgs) -> Result<()> {
     match args.command {
-        SetupCommands::Dns => {
-            println!("Setting up DNS...");
-            Dns::setup()?;
-            Ok(())
-        }
-        SetupCommands::Caddy => {
-            println!("Setting up Caddy...");
-            Caddy::setup()?;
-            Ok(())
-        }
-        SetupCommands::All => {
-            println!("Setting up all ");
-            Ok(())
+        Some(cmd) => match cmd {
+            SetupCommand::Dns => {
+                println!("Setting up DNS...");
+                Dns::setup()?;
+            }
+            SetupCommand::Caddy => {
+                println!("Setting up Caddy...");
+                Caddy::setup()?;
+            }
+        },
+        None => {
+            if get_effective_uid() == 0 {
+                Dns::setup()?;
+                Caddy::setup()?;
+            } else {
+                anyhow::bail!("This operation requires root privileges.");
+            }
         }
     }
+
+    Ok(())
 }
